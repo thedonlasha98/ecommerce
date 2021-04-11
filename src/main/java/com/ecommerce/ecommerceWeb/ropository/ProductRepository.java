814a86a9a176ebab088d0327a5b9a5416a08b150
thedonlasha98 <thedonlasha@gmail.com>
2021-04-11 16:28:24 +0400

    product sevices

--- src/main/java/com/ecommerce/ecommerceWeb/ropository/ProductRepository.java
+++ src/main/java/com/ecommerce/ecommerceWeb/ropository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.ecommerce.ecommerceWeb.ropository;
 
 import com.ecommerce.ecommerceWeb.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
     Product findByProductAndUserId(String product, Long userId);
+
+    Product findByIdAndUserId(Long id, Long userId);
+
 }
--- src/main/java/com/ecommerce/ecommerceWeb/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/ecommerceWeb/service/ProductServiceImpl.java
@@ -23,6 +23,7 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public String addProduct(ProductDto productDto){
+        String result = "გადაამოწმეთ არსებული პროდუქტები!";
         Product productExt = productRepository.findByProductAndUserId(productDto.getProduct(),productDto.getUserId());
         if (productExt == null){
             Product product = new Product();
@@ -46,7 +47,114 @@ public class ProductServiceImpl implements ProductService{
             productHist.setInpSysDate(LocalDateTime.now());
 
             productHistRepository.save(productHist);
+
+            result = "პროდუქტი წარმატებით დარეგისტრირდა!";
+        }else if(productExt.getStatus().equals("A")){
+            productExt.setQuantity(productExt.getQuantity() + productDto.getQuantity());
+            productRepository.save(productExt);
+            //create log
+            ProductHist productHist = new ProductHist();
+            productHist.setProductId(productExt.getId());
+            productHist.setUserId(productDto.getUserId());
+            productHist.setProduct(productDto.getProduct());
+            productHist.setPhoto(productDto.getPhoto());
+            productHist.setPrice(productDto.getPrice());
+            productHist.setQuantity(productDto.getQuantity());
+            productHist.setStatus("A");
+            productHist.setEvent("ADD_CURRENT");
+            productHist.setInpSysDate(LocalDateTime.now());
+
+            productHistRepository.save(productHist);
+
+            result = "არსებული პროდუქტის რაოდენობა გაიზარდა " + productDto.getQuantity() + "-ით!";
         }
-        return "პროდუქტი წარმატებით დარეგისტრირდა";
+            return result;
     }
+
+    public String modifyProduct(ProductDto productDto){
+        String result = "შეცდომა!";
+        Product product = productRepository.findByProductAndUserId(productDto.getProduct(),productDto.getUserId());
+        if (product == null){
+            product.setProduct(productDto.getProduct());
+            product.setPhoto(productDto.getPhoto());
+            product.setPrice(productDto.getPrice());
+            product.setQuantity(productDto.getQuantity());
+            product.setStatus("A");
+            productRepository.save(product);
+            //create log
+            ProductHist productHist = new ProductHist();
+            productHist.setProductId(product.getId());
+            productHist.setUserId(productDto.getUserId());
+            productHist.setProduct(productDto.getProduct());
+            productHist.setPhoto(productDto.getPhoto());
+            productHist.setPrice(productDto.getPrice());
+            productHist.setQuantity(productDto.getQuantity());
+            productHist.setStatus("A");
+            productHist.setEvent("MODIFY");
+            productHist.setInpSysDate(LocalDateTime.now());
+
+            productHistRepository.save(productHist);
+
+            result = "პროდუქტი რედაქტირებულია!";
+        }
+            return result;
+    }
+
+    public void deleteProducts(Long id, Long userId){
+        Product product = productRepository.findByIdAndUserId(id, userId);
+        product.setStatus("D");
+        productRepository.save(product);
+        //create log
+        ProductHist productHist = new ProductHist();
+        productHist.setProductId(product.getId());
+        productHist.setUserId(userId);
+        productHist.setProduct(product.getProduct());
+        productHist.setPhoto(product.getPhoto());
+        productHist.setPrice(product.getPrice());
+        productHist.setQuantity(product.getQuantity());
+        productHist.setStatus("D");
+        productHist.setEvent("DELETE");
+        productHist.setInpSysDate(LocalDateTime.now());
+
+        productHistRepository.save(productHist);
+    }
+
+    public void closeProducts(Long id, Long userId){
+        Product product = productRepository.findByIdAndUserId(id, userId);
+        product.setStatus("C");
+        productRepository.save(product);
+        //create log
+        ProductHist productHist = new ProductHist();
+        productHist.setProductId(product.getId());
+        productHist.setUserId(userId);
+        productHist.setProduct(product.getProduct());
+        productHist.setPhoto(product.getPhoto());
+        productHist.setPrice(product.getPrice());
+        productHist.setQuantity(product.getQuantity());
+        productHist.setStatus("C");
+        productHist.setEvent("CLOSE");
+        productHist.setInpSysDate(LocalDateTime.now());
+
+        productHistRepository.save(productHist);
+    }
+
+    public void activateProducts(Long id, Long userId){
+        Product product = productRepository.findByIdAndUserId(id, userId);
+        product.setStatus("A");
+        productRepository.save(product);
+        //create log
+        ProductHist productHist = new ProductHist();
+        productHist.setProductId(product.getId());
+        productHist.setUserId(userId);
+        productHist.setProduct(product.getProduct());
+        productHist.setPhoto(product.getPhoto());
+        productHist.setPrice(product.getPrice());
+        productHist.setQuantity(product.getQuantity());
+        productHist.setStatus("A");
+        productHist.setEvent("ACTIVATE");
+        productHist.setInpSysDate(LocalDateTime.now());
+
+        productHistRepository.save(productHist);
+    }
+
 }
